b0a3364edbb78e24ea8d85af5b0ec3b66b482879
upenlele <44668308+upenlele@users.noreply.github.com>
2026-02-05 17:59:50 +0530

    Update config_template.py

--- config_template.py
+++ config_template.py
@@ -2,10 +2,10 @@
 # Copy this file to config.py and update with your actual credentials
 
 DB_CONFIG = {
-    'host': 'localhost',           # MySQL server hostname or IP
-    'user': 'phpmyadmin',                # MySQL username
-    'password': '123',   # MySQL password - CHANGE THIS!
-    'database': 'test',       # Database name
+    'host': 'https://sv51.byethost51.org:2083/cpsess5207811907/3rdparty/phpMyAdmin/index.php?route=/database/structure&db=upendral_classicmodels',           # MySQL server hostname or IP
+    'user': 'root',                # MySQL username
+    'password': 'Bhugaon123$',   # MySQL password - CHANGE THIS!
+    'database': 'upendral_classicmodels',       # Database name
     'port': 3306,                  # MySQL port (default: 3306)
     'raise_on_warnings': True      # Raise exceptions on MySQL warnings
 }
